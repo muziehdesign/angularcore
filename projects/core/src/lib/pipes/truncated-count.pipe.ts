9ba0c5ded4eb81ef011ec20cd719aef9d896a195
ruifang <ruifang.li09@gmail.com>
2024-08-23 13:32:01 -0600

    prefix pipe

--- projects/core/src/lib/pipes/truncated-count.pipe.ts
+++ projects/core/src/lib/pipes/truncated-count.pipe.ts
@@ -1,7 +1,7 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'truncatedCount',
+  name: 'mzTruncatedCount',
   standalone: true
 })
 export class TruncatedCountPipe implements PipeTransform {
