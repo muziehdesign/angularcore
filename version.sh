82c13178b22c1fd0933ee36162e346ad2e04aea0
ruifang <ruifang.li09@gmail.com>
2024-12-16 14:15:04 -0700

    update versioning logic

--- version.sh
+++ version.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
+#!/bin/sh
+
+set -e
 
 BUILD=$1
 BRANCH=$2
@@ -8,9 +10,33 @@ MAJOR=$(echo "$VERSION" | awk -F '[/.]' '{ print $1 }')
 MINOR=$(echo "$VERSION" | awk -F '[/.]' '{ print $2 }')
 PATCH=$BUILD
 
-if [ "$BRANCH" = "master" ] || [ "$BRANCH" = "develop" ] ||  [[ "$BRANCH" = \release\/* ]]
+PACKAGEVERSION=$VERSION
+TAG=$MAJOR
+
+
+if (echo "$BRANCH" | grep -q "^release") || [ "$CIRCLE_BRANCH" = "develop" ] || [ "$CIRCLE_BRANCH" = "next" ];
 then
-    npm version "${MAJOR}.${MINOR}.${PATCH:="0"}" --no-commit-hooks --no-git-tag-version
+    PACKAGEVERSION="${MAJOR}.${MINOR}.${PATCH:="0"}"
 else
-    npm version "${MAJOR}.${MINOR}.${PATCH:="0"}-beta" --no-commit-hooks --no-git-tag-version
+    PACKAGEVERSION="${MAJOR}.${MINOR}.${PATCH:="0"}-beta"
+    TAG="beta"
 fi
+
+echo "PACKAGEVERSION=$PACKAGEVERSION"
+echo "TAG=$TAG"
+
+echo "$PACKAGEVERSION" > foo.txt
+echo "$TAG" >> foo.txt
+
+DIRECTORIES="./ ./projects/components ./projects/design"
+
+for DIR in $DIRECTORIES; do
+  if [ -d "$DIR" ]; then
+    echo "Entering $DIR"
+    cd "$DIR" || exit  # Navigate into the directory
+    npm version "${PACKAGEVERSION}" --no-commit-hooks --no-git-tag-version
+    cd - || exit  # Return to the previous directory
+  else
+    echo "Directory $DIR does not exist. Skipping..."
+  fi
+done
