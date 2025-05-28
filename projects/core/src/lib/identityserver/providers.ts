a8d67cf48ae1336f6971d0d015915bcfd4633bf3
ruifang <ruifang.li09@gmail.com>
2025-05-28 15:21:43 -0600

    allow additional providers

--- projects/core/src/lib/identityserver/providers.ts
+++ projects/core/src/lib/identityserver/providers.ts
@@ -31,7 +31,7 @@ export const DEFAULT_USER_MANAGER_PROVIDER: FactoryProvider = {
     deps: [AUTHENTICATION_OPTIONS, [new Optional(), OIDC_REDIRECT_NAVIGATOR], [new Optional(), OIDC_POPUP_NAVIGATOR], [new Optional(), OIDC_IFRAME_NAVIGATOR]]
 };
 
-export function provideAuthentication(configFn: (injector: Injector) => AuthenticationOptions) : EnvironmentProviders {
+export function provideAuthentication(configFn: (injector: Injector) => AuthenticationOptions, extraProviders: Provider[] = []) : EnvironmentProviders {
 
     console.log('providing');
     const providers: Provider[] = [
@@ -45,7 +45,10 @@ export function provideAuthentication(configFn: (injector: Injector) => Authenti
         },
         DEFAULT_USER_MANAGER_PROVIDER,
         AuthenticationService,
+        ...extraProviders
     ];
+
+
 
     return makeEnvironmentProviders(providers);
 };
