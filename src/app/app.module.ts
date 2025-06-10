b2a5658385db534b81d47c4d458195fd4eed0742
ruifang <ruifang.li09@gmail.com>
2025-06-10 11:39:02 -0600

    standalone component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AuthenticationService, AuthorizationService, provideAuthentication } fr
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { CoreModule } from './core/core.module';
 import { LayoutModule } from './layout/layout.module';
 import { ShoppingCartClient } from './api/shopping-cart/shopping-cart.client';
@@ -13,7 +12,7 @@ import { map } from 'rxjs';
 import { AppConfig } from 'src/environments/app-config';
 
 @NgModule({
-    declarations: [AppComponent, PageNotFoundComponent],
+    declarations: [AppComponent],
     bootstrap: [AppComponent],
     imports: [
         BrowserModule,
