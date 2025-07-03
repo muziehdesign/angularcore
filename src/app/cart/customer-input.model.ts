26a1ed9b523a857c4ad67ba9a78c8b215e2ccb80
ruifang <ruifang.li09@gmail.com>
2025-07-03 10:51:05 -0600

    remove interdependency

--- src/app/cart/customer-input.model.ts
+++ src/app/cart/customer-input.model.ts
@@ -1,13 +1,6 @@
-import { required, StringType } from "@muziehdesign/forms";
-
 export class CustomerInputModel {
-    
-    @StringType(required())
     firstName?: string;
-    @StringType(required())
     lastName?: string;
-    @StringType(required())
     emailAddress?: string;
-    @StringType(required())
     birthdate?: string;
 }
--- src/app/item/item-detail/item-detail.component.spec.ts
+++ src/app/item/item-detail/item-detail.component.spec.ts
@@ -1,6 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ActivatedRoute } from '@angular/router';
-import { NgFormModelStateFactory } from '@muziehdesign/forms';
 import { of } from 'rxjs';
 import { ItemFacade } from '../item.facade';
 import { ItemDetailComponent } from './item-detail.component';
@@ -12,14 +11,12 @@ describe('ItemDetailComponent', () => {
 
   beforeEach(async () => {
       const itemFacade = jasmine.createSpyObj<ItemFacade>(ItemFacade.name, { getItem: of(), getItems: of() });
-      const stateFactory = jasmine.createSpyObj<NgFormModelStateFactory>(NgFormModelStateFactory.name, ['create']);
 
       await TestBed.configureTestingModule({
           imports: [ItemDetailComponent],
           providers: [
               { provide: ActivatedRoute, useValue: { snapshot: { params: sku } } },
-              { provide: ItemFacade, useValue: itemFacade },
-              { provide: NgFormModelStateFactory, useValue: stateFactory }
+              { provide: ItemFacade, useValue: itemFacade }
           ],
       }).compileComponents();
 
